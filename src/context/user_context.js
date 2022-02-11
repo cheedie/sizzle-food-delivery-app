308b70508ad082482557e079c6ab0b192d3393bf
cheedieebeogu <cheedie.ebeogu@gmail.com>
2022-02-11 20:57:10 +0100

    auth functionality

--- src/context/user_context.js
+++ src/context/user_context.js
@@ -3,8 +3,27 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 const UserContext = React.createContext();
 export const UserProvider = ({ children }) => {
+  const [myUser, setMyUser] = useState(null);
+  const {
+    isAuthenticated,
+    loginWithRedirect,
+    logout,
+    user,
+    isLoading,
+  } = useAuth0();
+
+  useEffect(() => {
+    if (isAuthenticated) {
+      setMyUser(user);
+    } else {
+      setMyUser(false);
+    }
+  }, [isAuthenticated]);
+
   return (
-    <UserContext.Provider value="user context">{children}</UserContext.Provider>
+    <UserContext.Provider value={{ loginWithRedirect, logout, myUser }}>
+      {children}
+    </UserContext.Provider>
   );
 };
 
